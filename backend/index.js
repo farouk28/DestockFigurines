56f05c7e5c12d6852703e6bec8c5e6f39315f76e
farouk <farouklanani@gmail.com>
2024-10-19 21:26:55 +0200

    remove test

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(session({
 }));
 
 app.use(express.json());
-app.use(express.static('frontend/build'))
+// app.use(express.static('frontend/build'))
 app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
