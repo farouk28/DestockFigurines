22167afe72203593a2d6411390f9381e6b497082
farouk <farouklanani@gmail.com>
2024-10-19 15:58:44 +0200

    fifth commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-const port = 4000;
+const port = process.env.PORT || 4000;
+require('dotenv').config()
 const express = require("express");
 const session = require('express-session');
 const app = express();
